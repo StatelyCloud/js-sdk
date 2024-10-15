2f5702331ac334876a6c93eb018da92e961ce618
StatelyCloud <Release Bot infra@stately.cloud>
2024-10-15 23:59:10 +0000

    Update from https://github.com/StatelyCloud/stately/commit/f54e1d8f27c74b80bfe6e3cfd1867d53a6dc761c

--- src/stately_codes.ts
+++ src/stately_codes.ts
@@ -7,54 +7,6 @@
  */
 export const enum ErrorCode {
   /**
-   * StoreRequestLimitExceeded indicates that an attempt to modify a Store has
-   * been temporarily rejected due to exceeding global modification limits.
-   * StatelyDB has been notified about this error and will take necessary
-   * actions to correct it. In the event that the issue has not been resolved,
-   * please contact support.
-   *
-   * - Retryable? Yes.
-   */
-  StoreRequestLimitExceeded = "StoreRequestLimitExceeded",
-
-  /**
-   * StoreThroughputExceeded indicates that the underlying Store does not have
-   * resources to complete the request. This may indicate a request rate is too
-   * high to a specific Group or that a sudden burst of traffic has exceeded a
-   * Store's provisioned capacity.
-   *
-   * - Retryable? Yes.
-   *     With an exponential backoff.
-   */
-  StoreThroughputExceeded = "StoreThroughputExceeded",
-
-  /**
-   * ConditionalCheckFailed indicates that conditions provided to perform an
-   * operation were not met. For example, a condition to write an item only if
-   * it does not already exist. In the future StatelyDB may provide more
-   * information about the failed condition; if this feature is a blocker,
-   * please contact support.
-   *
-   * - Retryable? No.
-   *     Typically a conditional check failure is not retryable
-   *     unless the conditions for the operation are changed.
-   */
-  ConditionalCheckFailed = "ConditionalCheckFailed",
-
-  /**
-   * NonRecoverableTransaction indicates that conditions required for the
-   * transaction to succeed are not possible to meet with the current state of
-   * the system. This can occur when an Item has more than one key-path, and is
-   * written with a "must not exist" condition (e.g. with ID Generation on one
-   * of the keys) but another keys already maps to an existing item in the
-   * store. Permitting such a write would result in conflicting state; two
-   * independent records with aliases pointing to the same item.
-   *
-   * - Retryable? No.
-   */
-  NonRecoverableTransaction = "NonRecoverableTransaction",
-
-  /**
    * ConcurrentModification indicates the current transaction was aborted
    * because of a non-serializable interaction with another transaction was
    * detected, a stale read was detected, or because attempts to resolve an
@@ -79,18 +31,66 @@ export const enum ErrorCode {
    * 3. A read or list operation detected that underlying data has changed
    * since the transaction began.
    *
-   * - Retryable? Yes.
+   * - Retryable
    *     This error is immediately retryable.
    */
   ConcurrentModification = "ConcurrentModification",
+
+  /**
+   * ConditionalCheckFailed indicates that conditions provided to perform an
+   * operation were not met. For example, a condition to write an item only if
+   * it does not already exist. In the future StatelyDB may provide more
+   * information about the failed condition; if this feature is a blocker,
+   * please contact support.
+   *
+   * - Not Retryable
+   *     Typically a conditional check failure is not retryable
+   *     unless the conditions for the operation are changed.
+   */
+  ConditionalCheckFailed = "ConditionalCheckFailed",
+
+  /**
+   * NonRecoverableTransaction indicates that conditions required for the
+   * transaction to succeed are not possible to meet with the current state of
+   * the system. This can occur when an Item has more than one key-path, and is
+   * written with a "must not exist" condition (e.g. with ID Generation on one
+   * of the keys) but another keys already maps to an existing item in the
+   * store. Permitting such a write would result in conflicting state; two
+   * independent records with aliases pointing to the same item.
+   *
+   * - Not Retryable
+   */
+  NonRecoverableTransaction = "NonRecoverableTransaction",
 
   /**
    * StoreInUse indicates that the underlying Store is currently in being
    * updated and cannot be modified until the operation in progress has
    * completed.
    *
-   * - Retryable? Yes.
+   * - Retryable
    *     This can be retried with backoff.
    */
   StoreInUse = "StoreInUse",
+
+  /**
+   * StoreRequestLimitExceeded indicates that an attempt to modify a Store has
+   * been temporarily rejected due to exceeding global modification limits.
+   * StatelyDB has been notified about this error and will take necessary
+   * actions to correct it. In the event that the issue has not been resolved,
+   * please contact support.
+   *
+   * - Retryable
+   */
+  StoreRequestLimitExceeded = "StoreRequestLimitExceeded",
+
+  /**
+   * StoreThroughputExceeded indicates that the underlying Store does not have
+   * resources to complete the request. This may indicate a request rate is too
+   * high to a specific Group or that a sudden burst of traffic has exceeded a
+   * Store's provisioned capacity.
+   *
+   * - Retryable
+   *     With an exponential backoff.
+   */
+  StoreThroughputExceeded = "StoreThroughputExceeded",
 }
